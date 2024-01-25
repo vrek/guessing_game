0997b215782b6612657d89af349514590f52dd43
Raymond Koons <ray.koons@gmail.com>
2024-01-25 13:18:16 -0600

    guessing game basically works

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,31 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
-fn main(){
+fn main()
+{
     println!("Guess the number!");
     
     let answer = rand::thread_rng().gen_range(1..=100);
+    loop
+    {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    
-    match guess.cmp(&answer){
-        Ordering::Equal => println!("You Win!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Less => println!("Too small")
+        let guess: u32 = match guess.trim().parse() {
+            Ok(guess) => guess,
+            Err(_) => continue,
+        };
+        match guess.cmp(&answer){
+            Ordering::Equal => {
+                println!("You Win!");
+                 break;},
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small")
+        }
     }
-    println!("The answer was {answer}");
-    println!("You guessed: {guess}");
 }
